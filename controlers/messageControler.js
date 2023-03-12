4152ffc22c0df56ad476d1da83182e4c0c3f2084
lalit <lalit.rishikalyan@gmail.com>
2023-03-12 21:24:58 +0530

    lil

--- controlers/messageControler.js
+++ controlers/messageControler.js
@@ -2,23 +2,24 @@ const Member = require("../models/Member");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const SID = process.env.SID;
-const AUTH_TOKEN = process.env.T_TOKEN;
+// const SID = process.env.SID;
+// const AUTH_TOKEN = process.env.T_TOKEN;
 // console.log("SID:", SID);
 // console.log("TOKEN :", AUTH_TOKEN);
-const client = require("twilio")(SID, AUTH_TOKEN);
+//const client = require("twilio")(SID, AUTH_TOKEN);
 
 const singleMessage = async (req, res) => {
-  const { text, mobile } = req.body;
+  const { mobile } = req.body;
 
   try {
     // const msgData = await client.messages.create({
     //   body: "your membership plan has EXPIRED..... please pay your FEES.....if you piad already then ask for activation...__BUSY_FORCE.com",
     //   from: "+18647345481",
-    //   to: "+917206169603",
+    //   to: `+91${mobile}`,
     // });
+    console.log( mobile);
 
-    res.status(200).json("messagesent");
+    res.status(200).json("messagesent", { "hello mitthu": mobile });
   } catch (error) {
     res.status(401).json(error);
   }
@@ -26,24 +27,29 @@ const singleMessage = async (req, res) => {
 
 const bulkMsg = async (req, res) => {
   const { text } = req.body;
-  if (text) {
+  if (text !== " ") {
     try {
-      // const membersList = await Member.find({}, { phone: 1 });
+      const membersList = await Member.find({}, { phone: 1 });
+      //console.log(membersList);
 
-      // for (let m = 0; m <= membersList.length; m++) {
-      //   if (membersList[m]) {
-      //     try {
-      //       await client.messages.create({
-      //         body: `${text}`,
-      //         from: "+18647345481",
-      //         to: `+91${membersList[m].phone}`,
-      //       });
-      //     } catch (error) {
-      //       console.log(error);
-      //     }
-      //   }
-      // }
-      res.status(200).json("msg sent to all");
+      for (let m = 0; m <= membersList.length; m++) {
+        if (membersList[m]) {
+          try {
+            // await client.messages.create({
+            //   body: `${text}`,
+            //   from: "+18647345481",
+            //   to: `+91${membersList[m].phone}`,
+            // });
+            console.log({ "No.": membersList[m].phone, Text: text });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
+      res.status(200).json({
+        Msg: "msg sent to all",
+        "text-msg ": text,
+      });
     } catch (error) {
       res.status(401).json(error);
     }
